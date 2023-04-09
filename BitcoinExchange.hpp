e4f5d7e4a23c70e5f06e4fc090fdf924c57961f2
younex01 <younesyounex1@gmail.com>
2023-04-09 09:38:04 +0000

    almost finishing ex00

--- BitcoinExchange.hpp
+++ BitcoinExchange.hpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   BitcoinExchange.hpp                                :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yelousse <yelousse@student.42.fr>          +#+  +:+       +#+        */
+/*   By: youness <youness@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 14:46:02 by yelousse          #+#    #+#             */
-/*   Updated: 2023/04/08 14:41:57 by yelousse         ###   ########.fr       */
+/*   Updated: 2023/04/09 08:21:47 by youness          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,7 @@
 #include <fstream>
 #include <string>
 #include <map>
+#include <cstdlib>
 
 
 #endif
--- main.cpp
+++ main.cpp
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.cpp                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yelousse <yelousse@student.42.fr>          +#+  +:+       +#+        */
+/*   By: youness <youness@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/07 14:46:08 by yelousse          #+#    #+#             */
-/*   Updated: 2023/04/08 16:06:00 by yelousse         ###   ########.fr       */
+/*   Updated: 2023/04/09 09:35:54 by youness          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,18 @@ int main(int ac, char **av)
     if (ac == 2)
     {
         std::string line, token;
-        std::ifstream input_file("data.csv");
+        std::ifstream input_data("data.csv");
         std::map<std::string, float> data;
         std::string filename = av[1];
-        std::ifstream input_file(filename);
-        if (!input_file.is_open())
+        // std::cout << filename << std::endl;
+        std::ifstream input_file(filename.c_str());
+        if (!input_data.is_open())
         {
             std::cout << "Error opening data.csv " << std::endl;
             return 1;
         }
-        std::getline(input_file, line);
-        while(std::getline(input_file, line))
+        std::getline(input_data, line);
+        while(std::getline(input_data, line))
         {
             size_t pos = 0;
             std::string date,value;
@@ -40,7 +41,7 @@ int main(int ac, char **av)
                 data[date] = std::atof(value.c_str());
             }
         }
-        input_file.close();
+        input_data.close();
         // std::map<std::string, float>::iterator it;
         // it = data.begin();
         // while(it != data.end())
@@ -53,9 +54,37 @@ int main(int ac, char **av)
             std::cout << "Error opening file " << filename << std::endl;
             return 1;
         }
+        std::getline(input_file, line);
         while(std::getline(input_file, line))
         {
-            std::cout << line << std::endl;
+            size_t pos = 0;
+            std::string date, value;
+            float nb, result;
+            while ((pos = line.find("|")) != std::string::npos) {
+                date = line.substr(0, pos);
+                value = line.erase(0, pos + 1);
+                nb = std::atof(value.c_str());
+                if (nb > 1000)
+                    std::cout << "Error: too large a number." << std::endl;
+                else if (nb < 0)
+                    std::cout << "Error: not a positive number." << std::endl;
+                else
+                {
+                    if (data[date] == 0)
+                    {
+                        std::map<std::string, float>::iterator it;
+                        it = data.find(date);
+                        it--;
+                        if (it != data.end())
+                            result = nb * it->second;
+                            // std::cout << it->second << std::endl;
+                    }
+                    else
+                        result = nb * data[date];
+                    std::cout << date << " => " << value << " = " << result << std::endl;
+                }
+            }
+            // std::cout << line << std::endl;
         }
         input_file.close();
     }
